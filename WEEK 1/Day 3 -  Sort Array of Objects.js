94f64a74e32ae8f9ed93636b772f7454d7d0f58e
shadow-prince <59025976+shadow-prince@users.noreply.github.com>
2020-12-25 16:20:24 +0530

    removed jsons

---
+++ WEEK 1/Day 3 -  Sort Array of Objects.js
@@ -0,0 +1,31 @@
+function sortLibrary() {
+    // var library is defined, use it in your code
+    // use console.log(library) to output the sorted library data
+    library.sort(function(obj1,obj2){
+        return obj1.title.localeCompare(obj2.title);
+    });
+    console.log(library);
+}
+
+// tail starts here
+var library = [
+    {
+        author: 'Bill Gates',
+        title: 'The Road Ahead',
+        libraryID: 1254
+    },
+    {
+        author: 'Steve Jobs',
+        title: 'Walter Isaacson',
+        libraryID: 4264
+    },
+    {
+        author: 'Suzanne Collins',
+        title: 'Mockingjay: The Final Book of The Hunger Games',
+        libraryID: 3245
+    }
+];
+console.log('Before')
+console.log(library);
+console.log('After')
+sortLibrary();
